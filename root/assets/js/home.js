de27638556c4f1e3d20a023c99653944e7e992a6
ZackTarantinoWoolson <15880559+ZackTarantinoWoolson@users.noreply.github.com>
2023-03-05 09:53:51 -0500

    light/dark switcher

--- root/assets/js/home.js
+++ root/assets/js/home.js
@@ -1,23 +1,30 @@
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const toggleSwitch = document.getElementById('theme-switch-button');
+// const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+console.log("toggle",toggleSwitch)
 const currentTheme = localStorage.getItem('theme');
+console.log("currentTheme",currentTheme)
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
+        document.getElementById('theme-image').src="/root/assets/images/ThemeSwitcher/sunny.png"
     }
 }
 
 function switchTheme(e) {
+    console.log("switch");
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        document.getElementById('theme-image').src="/root/assets/images/ThemeSwitcher/sunny.png"
         localStorage.setItem('theme', 'dark');
     }
     else {        
         document.documentElement.setAttribute('data-theme', 'light');
+        document.getElementById('theme-image').src="/root/assets/images/ThemeSwitcher/moon.png"
         localStorage.setItem('theme', 'light');
     }    
 }
 
-toggleSwitch.addEventListener('change', switchTheme, false);
+toggleSwitch.addEventListener('click', switchTheme, false);
