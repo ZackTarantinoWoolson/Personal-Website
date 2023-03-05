6fdd06bac64cc8286cb6b0568aedb7966cc26a83
ZackTarantinoWoolson <15880559+ZackTarantinoWoolson@users.noreply.github.com>
2023-03-05 16:45:22 -0500

    Nav bar images and js to match

--- root/assets/js/home.js
+++ root/assets/js/home.js
@@ -1,15 +1,19 @@
 const toggleSwitch = document.getElementById('theme-switch-button');
 // const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-console.log("toggle",toggleSwitch)
+console.log("toggle", toggleSwitch)
 const currentTheme = localStorage.getItem('theme');
-console.log("currentTheme",currentTheme)
+console.log("currentTheme", currentTheme)
 
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-  
+
     if (currentTheme === 'dark') {
         toggleSwitch.checked = true;
-        document.getElementById('theme-image').src="/root/assets/images/ThemeSwitcher/sunny.png"
+        document.getElementById('theme-image').src = "/root/assets/images/ThemeSwitcher/sunny.png"
+
+        var elms = document.querySelectorAll("#link-image")
+        for (var i = 0; i < elms.length; i++)
+            elms[i].src = "/root/assets/images/Links/external-link-white.png"
     }
 }
 
@@ -17,14 +21,25 @@ function switchTheme(e) {
     console.log("switch");
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        document.getElementById('theme-image').src="/root/assets/images/ThemeSwitcher/sunny.png"
+        document.getElementById('theme-image').src = "/root/assets/images/ThemeSwitcher/sunny.png"
+
+        var elms = document.querySelectorAll("#link-image")
+        for (var i = 0; i < elms.length; i++)
+            elms[i].src = "/root/assets/images/Links/external-link-white.png"
+
         localStorage.setItem('theme', 'dark');
     }
-    else {        
+    else {
         document.documentElement.setAttribute('data-theme', 'light');
-        document.getElementById('theme-image').src="/root/assets/images/ThemeSwitcher/moon.png"
+        document.getElementById('theme-image').src = "/root/assets/images/ThemeSwitcher/moon.png"
+
+        var elms = document.querySelectorAll("#link-image")
+        console.log(elms)
+        for (var i = 0; i < elms.length; i++)
+            elms[i].src = "/root/assets/images/Links/external-link-black.png"
+
         localStorage.setItem('theme', 'light');
-    }    
+    }
 }
 
 toggleSwitch.addEventListener('click', switchTheme, false);
